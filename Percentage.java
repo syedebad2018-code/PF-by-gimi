82e681e92fd8cc72d81a7d880d1835b05fc5edd4
syedebad2018-code <syedebad2018@gmail.com>
2026-01-11 23:43:15 +0500

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Percentage {
+    public static void main(String[] args) {
+        Scanner ki = new Scanner(System.in);
+        double percentage, number;
+        System.out.println("Enter maximum marks:");
+        double mm=ki.nextDouble();
+        do {
+            System.out.println("Enter obtained marks:");
+            double om=ki.nextDouble();
+            if (om<=mm) {
+                percentage=om/mm*100;
+                System.out.println("Your percentage is:");
+                System.out.println(percentage);
+                number=1;
+            } else {
+                System.out.println("Obtained marks cannot be greater than maximum marks.");
+                number=-1;
+            }
+        } while (number<0);
+    }
+}
